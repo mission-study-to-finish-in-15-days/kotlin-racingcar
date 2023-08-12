8a1902906958d6949a58adaf556a98e903dbd789
김종인 <kji6252@gmail.com>
2023-08-12 11:32:07 +0900

    Step2: 사칙 연산 만들기 | SymbolAndNumberQueue 일급 컬렉션 추가

--- src/main/kotlin/Calculable.kt
+++ src/main/kotlin/Calculable.kt
@@ -37,32 +37,52 @@ object CalcSelector {
     }
 }
 
+data class SymbolAndNumberQueue(private val inputString: String) {
+    private val symbolAndNumArray: MutableList<Char>
+
+    init {
+        val trimInput = inputString.trim()
+        val toCharArray = trimInput.toCharArray()
+        val toMutableList = toCharArray.toMutableList()
+        val filteredBlankList: MutableList<Char> = toMutableList.filter { ' ' != it }.toMutableList()
+        filteredBlankList.add(0, '+')
+        symbolAndNumArray = filteredBlankList
+    }
+
+    fun pop(): Char {
+        return symbolAndNumArray.removeAt(0)
+    }
+
+    fun popInt(): Int {
+        return symbolAndNumArray
+            .removeAt(0)
+            .digitToInt()
+    }
+
+    val size: Int get() = symbolAndNumArray.size
+}
+
 object StringCalculator {
 
     fun calculate(inputString: String? = null): String {
         require(inputString != null) { "null 이면 안됩니다." }
 
-        val trimInput = inputString.trim()
-        val toCharArray = trimInput.toCharArray()
-        val toMutableList = toCharArray.toMutableList()
-        val filteredBlankList = toMutableList.filter { ' ' != it }.toMutableList()
-        filteredBlankList.add(0, '+')
-
-        return divideAndCalc(filteredBlankList).toString()
+        return divideAndCalc(SymbolAndNumberQueue(inputString)).toString()
     }
 
     private val numberRegex: Regex = "\\d".toRegex()
-    private fun divideAndCalc(charMutableList: MutableList<Char>, prevInt: Int = 0): Int {
-        if (charMutableList.size == 0) return prevInt
+    private fun divideAndCalc(queue: SymbolAndNumberQueue, prevInt: Int = 0): Int {
+        if (queue.size == 0) return prevInt
 
-        val symbolC = charMutableList.removeAt(0)
+        val symbolC = queue.pop()
         val symbolType: SymbolType = SymbolType.symbolOf(symbolC)
-        val numberChar = charMutableList.removeAt(0)
+        val numberChar = queue.popInt()
         if (numberRegex.matches(numberChar.toString())) {
-            return divideAndCalc(charMutableList, CalcSelector.select(symbolType).calculate(prevInt, numberChar.toString().toInt()))
+            val calculable = CalcSelector.select(symbolType)
+            return divideAndCalc(queue, calculable.calculate(prevInt, numberChar))
         }
 
-        throw IllegalArgumentException("정상적이지 않는 문자열 $charMutableList")
+        throw IllegalArgumentException("정상적이지 않는 문자열 $queue")
     }
 }
 
