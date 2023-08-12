15bbb52840f7a92fba263238d5eba5fe22a2afc6
kycho91 <kycho91@i-nara.co.kr>
2023-08-13 02:30:53 +0900

    feat: [step2] 문자열 계산기 기본기능 구현

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -1,7 +1,39 @@
 package step2
 
+import java.util.LinkedList
+import java.util.Queue
+
 object StringCalculator {
-    fun calc(input: String): Any {
-        TODO("Not yet implemented")
+
+    private const val DELIMETER = " "
+    fun calculate(input: String?): Long {
+
+        require(!input.isNullOrBlank()) { "입력값이 null 또는 빈 문자열일 수 없습니다." }
+
+        val splitInput: Queue<String> = splitInput(input)
+
+        var result = splitInput.poll().toLong()
+
+        while (splitInput.isNotEmpty()) {
+            val operator = splitInput.poll()
+            val operand = splitInput.poll().toLong()
+
+            when (operator) {
+                "+" -> result += operand
+                "-" -> result -= operand
+                "*" -> result *= operand
+                "/" -> result /= operand
+                else -> throw IllegalArgumentException("사칙연산 기호는 +,-,*,/ 만 허용됩니다.")
+            }
+        }
+
+        return result
+    }
+
+    private fun splitInput(input: String): Queue<String> {
+
+        val splitInput = input.split(DELIMETER)
+
+        return LinkedList(splitInput)
     }
 }
--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -1,5 +1,6 @@
 package step2
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.WithDataTestName
 import io.kotest.datatest.withData
@@ -7,15 +8,43 @@ import io.kotest.matchers.shouldBe
 
 class StringCalculatorTest : FunSpec({
 
-    context("사칙 연산") {
+    context("사칙연산") {
         withData(
             StringCalculatorTestData("2 + 5", 7L),
-            StringCalculatorTestData("5 - 6", 1L),
+            StringCalculatorTestData("5 - 6", -1L),
             StringCalculatorTestData("5 * 6", 30L),
             StringCalculatorTestData("8 / 4", 2L),
             StringCalculatorTestData("2 + 3 * 4 / 2", 10L),
         ) { (input, result) ->
-            StringCalculator.calc(input) shouldBe result
+            StringCalculator.calculate(input) shouldBe result
+        }
+    }
+
+    context("입력값이 null 또는 빈 문자열인 경우 IllegalArgumentException throw") {
+        withData(
+            nameFn = { if (it == null) "input : $it" else "input : \"${it}\"" },
+            null,
+            "",
+            "         ",
+        ) { input ->
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
+            }
+            exception.localizedMessage shouldBe "입력값이 null 또는 빈 문자열일 수 없습니다."
+        }
+    }
+
+    context("사칙연산 기호가 아닌 경우 IllegalArgumentException throw") {
+        withData(
+            nameFn = { "input : $it" },
+            "2 = 5",
+            "2 a 5",
+            "2 ^ 5",
+        ) { input ->
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
+            }
+            exception.localizedMessage shouldBe "사칙연산 기호는 +,-,*,/ 만 허용됩니다."
         }
     }
 })
