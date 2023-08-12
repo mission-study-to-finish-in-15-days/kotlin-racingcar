5c2792daf9b6a64d0212397102ab3ced8901f34b
khcho902 <khcho902@gmail.com>
2023-08-12 21:09:15 +0900

    1단계 - 코틀린 기초 (#10)
    * feat: [step1] PersonKoTest 추가(실패상태)
    
    * feat: [step1] Person 구현 및 PersonKoTest 통과
    
    * feat: [step1] FuncSpec, BehaviorSpec 테스트 추가
    
    ---------
    
    Co-authored-by: kycho91 <kycho91@i-nara.co.kr>

---
+++ src/test/kotlin/step1/PersonKoTestFunSpec.kt
@@ -0,0 +1,66 @@
+package step1
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class PersonKoTestFunSpec : FunSpec({
+
+    test("이름 붙인 인자") {
+        val people = listOf(
+            Person("조경현", 33, "kycho"),
+            Person("조경현", 33, nickname = "kycho"),
+            Person(
+                name = "조경현",
+                nickname = "kycho",
+                age = 33
+            )
+        )
+
+        people.forAll {
+            it.name shouldBe "조경현"
+            it.age shouldBe 33
+            it.nickname shouldBe "kycho"
+        }
+    }
+
+    test("널 타입") {
+        val person = Person("조경현", 33, null)
+        person.name shouldBe "조경현"
+        person.age shouldBe 33
+        person.nickname shouldBe null
+    }
+
+    test("기본 인자") {
+        val person = Person("조경현", 33)
+        person.name shouldBe "조경현"
+        person.age shouldBe 33
+        person.nickname shouldBe "kycho"
+    }
+
+
+    context("데이터 클래스") {
+        val person = Person("조경현", 33, "kycho")
+
+        test("equals()") {
+            val person2 = Person("조경현", 33, "kycho")
+            person shouldBe person2
+        }
+
+        test("copy()") {
+            val copiedPerson = person.copy()
+
+            copiedPerson.name shouldBe "조경현"
+            copiedPerson.age shouldBe 33
+            copiedPerson.nickname shouldBe "kycho"
+        }
+
+        test("componentN()") {
+            val (name, age, nickname) = person
+
+            name shouldBe "조경현"
+            age shouldBe 33
+            nickname shouldBe "kycho"
+        }
+    }
+})
---
+++ src/test/kotlin/step1/PersonKoTestStringSpec.kt
@@ -0,0 +1,47 @@
+package step1
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class PersonKoTestStringSpec : StringSpec({
+
+    "이름 붙인 인자" {
+        val people = listOf(
+            Person("조경현", 33, "kycho"),
+            Person("조경현", 33, nickname = "kycho"),
+            Person(
+                name = "조경현",
+                nickname = "kycho",
+                age = 33
+            )
+        )
+
+        people.forAll {
+            it.name shouldBe "조경현"
+            it.age shouldBe 33
+            it.nickname shouldBe "kycho"
+        }
+    }
+
+    "널 타입" {
+        val person = Person("조경현", 33, null)
+        person.name shouldBe "조경현"
+        person.age shouldBe 33
+        person.nickname shouldBe null
+    }
+
+    "기본 인자" {
+        val person = Person("조경현", 33)
+        person.name shouldBe "조경현"
+        person.age shouldBe 33
+        person.nickname shouldBe "kycho"
+    }
+
+
+    "데이터 클래스" {
+        val person1 = Person("조경현", 33)
+        val person2 = Person("조경현", 33)
+        person1 shouldBe person2
+    }
+})
