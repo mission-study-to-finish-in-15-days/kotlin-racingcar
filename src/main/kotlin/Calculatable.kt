15defb4280a88fa3d6fe883e53a120d6ce2e20c8
김종인 <kji6252@gmail.com>
2023-08-12 11:13:22 +0900

    Step2: 사칙 연산 만들기 | 재귀 호출로 변경

--- src/main/kotlin/Calculatable.kt
+++ src/main/kotlin/Calculatable.kt
@@ -1,27 +1,27 @@
 sealed interface Calculatable {
-    fun calcultate(a: Int, b: Int): Int
+    fun calculrate(a: Int, b: Int): Int
 }
 
 object PlusCalc : Calculatable {
-    override fun calcultate(a: Int, b: Int): Int {
+    override fun calculrate(a: Int, b: Int): Int {
         return a + b
     }
 }
 
 object MinusCalc : Calculatable {
-    override fun calcultate(a: Int, b: Int): Int {
+    override fun calculrate(a: Int, b: Int): Int {
         return a - b
     }
 }
 
 object MultipleCalc : Calculatable {
-    override fun calcultate(a: Int, b: Int): Int {
+    override fun calculrate(a: Int, b: Int): Int {
         return a * b
     }
 }
 
 object DivideCalc : Calculatable {
-    override fun calcultate(a: Int, b: Int): Int {
+    override fun calculrate(a: Int, b: Int): Int {
         return a / b
     }
 }
@@ -43,24 +43,27 @@ object StringCalcurator {
     fun calculate(inputString: String? = null): String {
         require(inputString != null) { "null 이면 안됩니다." }
 
-        return divideAndCalc(inputString.toCharArray().toMutableList()).toString()
+        val trimInput = inputString.trim()
+        val toCharArray = trimInput.toCharArray()
+        val toMutableList = toCharArray.toMutableList()
+        val filteredBlankList = toMutableList.filter { ' ' != it }.toMutableList()
+        filteredBlankList.add(0, '+')
+
+        return divideAndCalc(filteredBlankList).toString()
     }
 
-    private fun divideAndCalc(charMutableList: MutableList<Char>): Int {
-        var prevInt: Int = 0
-        var prevSymbol: SymbolType = SymbolType.PLUS
+    private fun divideAndCalc(charMutableList: MutableList<Char>, prevInt: Int = 0): Int {
+        if (charMutableList.size == 0) return prevInt
 
-        for (c in charMutableList) {
-            val strC = c.toString()
-            when {
-                numberRegex.matches(strC) -> prevInt = CalcSelector.select(prevSymbol).calcultate(prevInt, strC.toInt())
-                strC.isBlank() -> continue
-                else -> prevSymbol = SymbolType.symbolOf(c)
-            }
+        val symbolC = charMutableList.removeAt(0)
+        val symbolType: SymbolType = SymbolType.symbolOf(symbolC)
+        val numberChar = charMutableList.removeAt(0)
+        if (numberRegex.matches(numberChar.toString())) {
+            return divideAndCalc(charMutableList, CalcSelector.select(symbolType).calculrate(prevInt, numberChar.toString().toInt()))
         }
-        return prevInt
-    }
 
+        throw IllegalArgumentException("정상적이지 않는 문자열 $charMutableList")
+    }
 }
 
 enum class SymbolType(private val c: Char) {
@@ -72,7 +75,8 @@ enum class SymbolType(private val c: Char) {
 
     companion object {
         fun symbolOf(c: Char): SymbolType {
-            return values().firstOrNull { it.c == c } ?: throw IllegalArgumentException("사칙 연산 기호 이외에는 들어오면 안됩니다.(c=$c)")
+            return values().firstOrNull { it.c == c }
+                ?: throw IllegalArgumentException("사칙 연산 기호 이외에는 들어오면 안됩니다.(c=$c)")
         }
     }
 }
