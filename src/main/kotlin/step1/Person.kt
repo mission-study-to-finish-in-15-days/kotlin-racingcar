4d8adca2930e9f343f9d3732d9e9fdc9e9760cb4
jeremy <jeremy@jeremyui-MacBookPro.local>
2023-08-13 23:55:24 +0900

    feat: age, nickname value 클래스 추가 (종인님 코드 참고)

--- src/main/kotlin/step1/Person.kt
+++ src/main/kotlin/step1/Person.kt
@@ -8,22 +8,28 @@ data class Person(
     근데 저 이렇게 코드 따오는게 혹시 실례가 되는 것인지 잘 몰라서 ㅠㅠ 관련해서 제가 구두로 한번 문의드리고 찾아뵙겠슴다 ㅠㅠ
     모르는게 많아여 잘 부탁드립니다 (__)
     오 이름만 쭉 따라가보았어요. 이런 세상이 있군여
+    와 테스트하려면 다른것도 다 정의가 되어야 하는군여 ..
      */
 
-//    val name:String,
-//    val age:Int,
-//    var nickname: String? = "윤코니"
-    private val _name: String
+    private val _name: String,
+    private val _age: Int,
+    private val _nickname: String? = "윤코니"
 ) {
     val name: String get() = nameInfo.name
+    val nickname: String? get() = nameInfo.nickname
+    val age: Int get() = ageDto.value
 
-    private val nameInfo: NameInfo = NameInfo(_name = Name(_name))
+
+    private val nameInfo: NameInfo = NameInfo(_name = Name(_name), _nickname = Nickname(_nickname))
+    private val ageDto: Age = Age(_age)
 }
 
 data class NameInfo(
-    val _name: Name
+    val _name: Name,
+    val _nickname: Nickname,
 ) {
     val name: String get() = _name.value
+    val nickname: String? get() = _nickname.value
 }
 
 @JvmInline
@@ -32,5 +38,20 @@ value class Name(
 ) {
     init {
         require(value.isNotEmpty()) {"이름은 비어있으며 아니되오"}
+    }
+}
+
+@JvmInline
+value class Nickname(
+    val value: String?,
+)
+
+
+@JvmInline
+value class Age(
+    val value: Int,
+) {
+    init {
+        require(value >= 0) {"나이는 0이하가 될수없어요!"}
     }
 }
