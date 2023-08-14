650a008254eff91b621e786687c299a1996ff922
jongin <kji6252@gmail.com>
2023-08-14 21:52:11 +0900

    Step2 사칙 연산 만들기 (#9)
    * 코틀린 버전업
    
    * Step2: 사칙 연산 만들기
    
    * Step2: 사칙 연산 만들기 | 재귀 호출로 변경
    
    * Step2: 사칙 연산 만들기 | 재귀 호출로 변경
    
    * Step2: 사칙 연산 만들기 | SymbolAndNumberQueue 일급 컬렉션 추가
    
    * Step2: 사칙 연산 만들기 | SymbolAndNumberQueue 일급 컬렉션 으로 로직 이동
    
    * Step2: 사칙 연산 만들기 | Int 원시 타입 value class Number로 랩핑, Calc이름 Calculator 풀네임으로 변경
    
    * Step2: 사칙 연산 만들기 | data driven Test 추가
    
    * Step2: 사칙 연산 만들기 | 이름 줄여쓴부분 풀네임으로 변경
    
    * Step2: 사칙 연산 만들기 | 이름 줄여쓴부분 풀네임으로 변경
    
    * Step2: 사칙 연산 만들기 | 테스트 코드 정렬

---
+++ src/main/kotlin/Calculable.kt
@@ -0,0 +1,133 @@
+sealed interface Calculable {
+    fun calculate(a: Number, b: Number): Number
+}
+
+object PlusCalculator : Calculable {
+    override fun calculate(a: Number, b: Number): Number {
+        return a + b
+    }
+}
+
+object MinusCalculator : Calculable {
+    override fun calculate(a: Number, b: Number): Number {
+        return a - b
+    }
+}
+
+object MultipleCalculator : Calculable {
+    override fun calculate(a: Number, b: Number): Number {
+        return a * b
+    }
+}
+
+object DivideCalculator : Calculable {
+    override fun calculate(a: Number, b: Number): Number {
+        return runCatching { a / b }
+            .getOrDefault(Number(0))
+    }
+}
+
+@JvmInline
+value class Number(private val value: Int)  {
+    operator fun plus(from: Number): Number {
+        return Number(this.value + from.value)
+    }
+
+    operator fun minus(from: Number): Number {
+        return Number(this.value - from.value)
+    }
+
+    operator fun times(from: Number): Number {
+        return Number(this.value * from.value)
+    }
+
+    operator fun div(from: Number): Number {
+        return Number(this.value / from.value)
+    }
+
+    override fun toString(): String {
+        return "$value"
+    }
+}
+
+object CalculableSelector {
+    fun select(symbolType: SymbolType): Calculable {
+        return when (symbolType) {
+            SymbolType.PLUS -> PlusCalculator
+            SymbolType.MINUS -> MinusCalculator
+            SymbolType.MULTIPLE -> MultipleCalculator
+            SymbolType.DIVIDE -> DivideCalculator
+        }
+    }
+}
+
+data class SymbolAndNumberQueue(private val inputString: String) {
+    private val symbolAndNumArray: MutableList<Char>
+
+    init {
+        val trimInput = inputString.trim()
+        val toCharArray = trimInput.toCharArray()
+        val toMutableList = toCharArray.toMutableList()
+        val filteredBlankList: MutableList<Char> = toMutableList.filter { ' ' != it }.toMutableList()
+        filteredBlankList.add(0, '+')
+        symbolAndNumArray = filteredBlankList
+    }
+
+    fun popSymbolAndNumber(): SymbolNumber {
+        return SymbolNumber(
+            symbolType = popSymbol(),
+            number = popNumber(),
+        )
+    }
+
+    private fun popSymbol(): SymbolType {
+        return SymbolType.charOf(
+            symbolAndNumArray.removeAt(0)
+        )
+    }
+
+    private fun popNumber(): Number {
+        return Number(symbolAndNumArray
+            .removeAt(0)
+            .digitToInt())
+    }
+
+    val size: Int get() = symbolAndNumArray.size
+}
+
+data class SymbolNumber(
+    val symbolType: SymbolType,
+    val number: Number,
+)
+
+object StringCalculator {
+
+    fun calculate(inputString: String? = null): String {
+        requireNotNull(inputString) { "null 이면 안됩니다." }
+
+        return divideAndCalculate(SymbolAndNumberQueue(inputString)).toString()
+    }
+
+    private fun divideAndCalculate(queue: SymbolAndNumberQueue, result: Number = Number(0)): Number {
+        if (queue.size == 0) return result
+
+        val (symbolType: SymbolType, number: Number) = queue.popSymbolAndNumber()
+        val calculable: Calculable = CalculableSelector.select(symbolType)
+        return divideAndCalculate(queue, calculable.calculate(result, number))
+    }
+}
+
+enum class SymbolType(private val symbolChar: Char) {
+    PLUS('+'),
+    MINUS('-'),
+    MULTIPLE('*'),
+    DIVIDE('/'),
+    ;
+
+    companion object {
+        fun charOf(inputChar: Char): SymbolType {
+            return values().firstOrNull { it.symbolChar == inputChar }
+                ?: throw IllegalArgumentException("사칙 연산 기호 이외에는 들어오면 안됩니다.(c=$inputChar)")
+        }
+    }
+}
---
+++ src/test/kotlin/CalcuratorTest.kt
@@ -0,0 +1,87 @@
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class CalcuratorTest : BehaviorSpec({
+
+    given("덧셈 연산일 경우") {
+        withData(
+            nameFn = { "${it.first} = ${it.second}" },
+            "2 + 3 + 5" to "10",
+            "2 + 4" to "6",
+        ) { (input, output) ->
+            StringCalculator.calculate(input) shouldBe output
+        }
+    }
+
+    given("뺄셈일 경우") {
+        withData(
+            nameFn = { "${it.first} = ${it.second}" },
+            "2 - 3 - 5" to "-6",
+            "2 - 4" to "-2",
+        ) { (input, output) ->
+            StringCalculator.calculate(input) shouldBe output
+        }
+
+    }
+
+    given("곱셈일 경우") {
+        withData(
+            nameFn = { "${it.first} = ${it.second}" },
+            "2 * 3 * 5" to "30",
+            "2 * 4" to "8",
+        ) { (input, output) ->
+            StringCalculator.calculate(input) shouldBe output
+        }
+
+    }
+
+
+    given("나눗셈일 경우") {
+        withData(
+            nameFn = { "${it.first} = ${it.second}" },
+            "6 / 2 / 2" to "1",
+            "4 / 2" to "2",
+        ) { (input, output) ->
+            StringCalculator.calculate(input) shouldBe output
+        }
+    }
+
+    given("복합 연산일 경우") {
+        withData(
+            nameFn = { "${it.first} = ${it.second}" },
+            "2 * 3 + 5" to "11",
+            "2 + 4 / 2 * 3" to "9",
+        ) { (input, output) ->
+            StringCalculator.calculate(input) shouldBe output
+        }
+    }
+
+    given("Exception 상활 이면") {
+        and("null 들어오면") {
+            val inputString = null
+            `when`("calcurate 호출 시") {
+                then("IllegalArgumentException 발생") {
+                    val shouldThrow = shouldThrow<IllegalArgumentException> {
+                        StringCalculator.calculate(inputString)
+                    }
+                    shouldThrow.localizedMessage shouldBe "null 이면 안됩니다."
+                }
+            }
+        }
+        and("사칙 연산 기호가 아닌게 들어오면") {
+            val inputString = "4 & 2"
+            `when`("calcurate 호출 시") {
+                then("IllegalArgumentException 발생") {
+                    val shouldThrow = shouldThrow<IllegalArgumentException> {
+                        StringCalculator.calculate(inputString)
+                    }
+                    shouldThrow.localizedMessage shouldBe "사칙 연산 기호 이외에는 들어오면 안됩니다.(c=&)"
+                }
+            }
+        }
+    }
+
+}) {
+}
