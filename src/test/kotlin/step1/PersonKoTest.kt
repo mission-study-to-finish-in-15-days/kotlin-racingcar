7febd751ab96723bd42b34af370e01b136a0bac2
Shin, Yun-Cheol <younchul.shin@gmail.com>
2023-08-16 21:31:28 +0900

    feat: kotest 통과 기념 커밋 (#7)
    * feat: kotest 통과 기념 커밋
    
    * feat: ktlint 다시 추가 (무슨일인지 모르겠지만 어제 안되던게 다 되네유)
    
    * feat: PersonFuncTest 코드 추가
    
    * style : 변수 뜻 주석 추가
    
    * feat: value class 추가(name만)
    
    * feat: age, nickname value 클래스 추가 (종인님 코드 참고)
    
    * feat: 테스트 코드 돌아가는지 확인~
    
    * fix : private 키워드 추가
    
    ---------
    
    Co-authored-by: jeremy <jeremy@jeremyui-MacBookPro.local>

---
+++ src/test/kotlin/step1/PersonKoTest.kt
@@ -0,0 +1,42 @@
+package step1
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class PersonKoTest : StringSpec({
+
+    "이름 붙인 인자" {
+        val people = listOf(
+            Person("신윤철", 28, "윤코니"),
+            Person("신윤철", 28, _nickname = "윤코니"),
+            Person(
+                _name =  "신윤철",
+                _nickname = "윤코니",
+                _age = 28
+            )
+        )
+
+        people.forAll {
+            it.name shouldBe "신윤철"
+        }
+    }
+
+    "널 타입" {
+        val person = Person("신윤철", 28, null)
+        person.name shouldBe "신윤철"
+        person.age shouldBe 28
+        person.nickname shouldBe null
+    }
+
+    "기본 인자" {
+        val person = Person("신윤철", 28)
+        person.name shouldBe "신윤철"
+        person.age shouldBe 28
+        person.nickname shouldBe "윤코니"
+    }
+    "데이터 클래스" {
+        val person1 = Person("신윤철", 28)
+        val person2 = Person("신윤철", 28)
+        person1 shouldBe person2
+    }
+})
