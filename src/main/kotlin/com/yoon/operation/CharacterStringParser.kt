e2ddab539bdbdaab630befc62824e12804e64a21
6161990 <6161990src@gmail.com>
2023-08-19 10:39:32 +0900

    fix: CharacterStringParser 에 OperationCommand 를 둔다. : CharacterStringParser 는 parsing 을 통해 연산 커맨드를 생성하는 책임을 가지고 있다.
    - tokens 을 통해 parsing 을 처리하면 연산 커맨드가 만들어진다.
    - 해당 커맨드는 연산자와 피연산자로 이루어져있다.

--- src/main/kotlin/com/yoon/operation/CharacterStringParser.kt
+++ src/main/kotlin/com/yoon/operation/CharacterStringParser.kt
@@ -2,22 +2,44 @@ package com.yoon.operation
 
 import java.util.ArrayDeque
 
+data class OperationCommand(val operands: ArrayDeque<Double>,
+                            val operations: ArrayDeque<Operation>)
+
 object CharacterStringParser {
 
-  fun parse(tokens: List<String>): Pair<ArrayDeque<Double>, ArrayDeque<Operation>> {
-    val operands: ArrayDeque<Double> = ArrayDeque()
-    val operations: ArrayDeque<Operation> = ArrayDeque()
+  fun parse(tokens: List<String>): OperationCommand {
+    val command = initializeCommand()
 
-    for (token in tokens) {
-      if (token.matches(Regex("-?\\d+(\\.\\d+)?"))) {
-        operands.addLast(token.toDouble())
-      } else {
-        operations.addLast(Operation.of(token))
-      }
+    processing(tokens, command)
+
+    require(isMatchOperation(command.operations, command.operands)) {
+      throw IllegalArgumentException("not accepted this operations input")
     }
 
-    require(operations.size == operands.size-1) { throw IllegalArgumentException("not accepted this operations input") }
-    return Pair(operands, operations)
+    return command
   }
 
+  private fun initializeCommand(): OperationCommand {
+    val operands = ArrayDeque<Double>()
+    val operations = ArrayDeque<Operation>()
+    return OperationCommand(operands, operations)
+  }
+
+  private fun processing(tokens: List<String>, command: OperationCommand) {
+    tokens.forEach { token ->
+      if (token.isNumber()) {
+        command.operands.addLast(token.toDouble())
+      } else {
+        command.operations.addLast(Operation.of(token))
+      }
+    }
+  }
+
+  private fun isMatchOperation(operations: ArrayDeque<Operation>, operands: ArrayDeque<Double>): Boolean {
+    return operations.size == operands.size - 1
+  }
+
+  private fun String.isNumber(): Boolean {
+    return matches(Regex("-?\\d+(\\.\\d+)?"))
+  }
 }
