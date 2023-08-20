a42af1950d1a06f12a5af513cb0b995e0818133d
khcho902 <khcho902@gmail.com>
2023-08-20 20:28:13 +0900

    refactor: [step3] RandomMoveStrategy -> RandomAboveThresholdMoveStrategy 변경(값을 외부에서 받아 더 범용적)

--- src/main/kotlin/racing_car/domain/Car.kt
+++ src/main/kotlin/racing_car/domain/Car.kt
@@ -2,7 +2,7 @@ package racing_car.domain
 
 class Car(
     private var _position: Int = 0,
-    private val _moveStrategy: MoveStrategy = RandomMoveStrategy()
+    private val _moveStrategy: MoveStrategy = RandomAboveThresholdMoveStrategy()
 ) {
 
     val position: Int
