ce40d8976efedc8a9f172ffb8e073eb66fa30a06
junuuuuuu <junuuu@i-nara.co.kr>
2023-08-20 16:51:06 +0900

    리뷰 반영 - 상수는 대문자로

--- src/main/kotlin/racingcar/input/InputView.kt
+++ src/main/kotlin/racingcar/input/InputView.kt
@@ -2,23 +2,23 @@ package racingcar.input
 
 object InputView {
 
-    private const val helloMessage = "안녕하세요 자동차 경주 게임에 온 것을 환영합니다."
-    private const val carNamesInputGuideMessage = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
-    private const val carNamesInputWaitingMessage = "입력 대기 중..."
-    private const val moveNumberInputGuideMessage = "자동차를 몇 회 이동할지 양의 정수를 입력 해주세요, 예시) 5"
-    private const val moveNumberInputWaitingMessage = "입력 대기 중..."
+    private const val HELLO_MESSAGE = "안녕하세요 자동차 경주 게임에 온 것을 환영합니다."
+    private const val CAR_NAME_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    private const val CAR_NAME_INPUT_WAITING_MESSAGE = "입력 대기 중..."
+    private const val MOVE_NUMBER_INPUT_GUIDE_MESSAGE = "자동차를 몇 회 이동할지 양의 정수를 입력 해주세요, 예시) 5"
+    private const val MOVE_NUMBER_INPUT_WAITING_MESSAGE = "입력 대기 중..."
 
     fun showHelloMessage(){
-        println(helloMessage)
+        println(HELLO_MESSAGE)
     }
 
     fun showCarNamesInputMessage(){
-        println(carNamesInputGuideMessage)
-        println(carNamesInputWaitingMessage)
+        println(CAR_NAME_INPUT_GUIDE_MESSAGE)
+        println(CAR_NAME_INPUT_WAITING_MESSAGE)
     }
 
     fun showMoveNumberInputMessage(){
-        println(moveNumberInputGuideMessage)
-        println(moveNumberInputWaitingMessage)
+        println(MOVE_NUMBER_INPUT_GUIDE_MESSAGE)
+        println(MOVE_NUMBER_INPUT_WAITING_MESSAGE)
     }
 }
