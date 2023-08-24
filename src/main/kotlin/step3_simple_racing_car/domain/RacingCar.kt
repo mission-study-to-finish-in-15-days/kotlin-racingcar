8f326886787e934fa6bc83039529920c41e05081
hyo321 <hyo321@i-nara.co.kr>
2023-08-24 13:03:36 +0900

    fix: position을 private으로 숨긴다.

--- src/main/kotlin/step3_simple_racing_car/domain/RacingCar.kt
+++ src/main/kotlin/step3_simple_racing_car/domain/RacingCar.kt
@@ -4,12 +4,15 @@ import step3_simple_racing_car.type.MovingDirectionType
 import step3_simple_racing_car.vo.Position
 
 class RacingCar(
-    var position: Position = Position(),
+    private var _position: Position = Position(),
     val nickName: NickName = NickName()
 ) {
     fun move(racingRoundMovingPolicy: RacingRoundMovingPolicy) {
         val policy = racingRoundMovingPolicy.makePolicy()
         if (policy.getMovingDirection() == MovingDirectionType.STOP) return
-        position = position.move(policy.getMovingCount())
+        _position = _position.move(policy.getMovingCount())
     }
+
+    val position: Position
+        get() = _position
 }
