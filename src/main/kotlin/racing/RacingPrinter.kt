3cabd91c55a222c22a58946ca619fa625ab3d2b2
moon9uakids <moon9uakids@i-nara.co.kr>
2023-08-24 16:30:42 +0900

    feat: 레이싱 이름 출력 구현

--- src/main/kotlin/racing/RacingPrinter.kt
+++ src/main/kotlin/racing/RacingPrinter.kt
@@ -1,23 +1,50 @@
 package racing
 
 object RacingPrinter {
-    fun printSimulation(result: List<List<Int>>) {
-        println()
-        println("실행 결과")
-
-        printResult(result)
+    fun printSimulation(racingResult: RacingResult) {
+        printHeader()
+        printResult(racingResult)
     }
 
-    private fun printResult(result: List<List<Int>>) {
-        result.forEach {
+    fun printSimulationWithName(racingResult: RacingResult) {
+        printHeader()
+        printResultWithName(racingResult)
+        printWinnerNames(racingResult)
+    }
+
+    private fun printHeader() {
+        println()
+        println("실행 결과")
+    }
+
+    private fun printResult(racingResult: RacingResult) {
+        racingResult.roundResults.forEach {
             printRound(it)
             println()
         }
     }
 
-    private fun printRound(roundResult: List<Int>) {
-        roundResult.forEach {
+    private fun printRound(roundResult: RacingRoundResult) {
+        return roundResult.value.forEach {
             println("-".repeat(it))
         }
+    }
+
+    private fun printResultWithName(racingResult: RacingResult) {
+        racingResult.roundResults.forEach {
+            printRoundWithName(it, racingResult.carNames)
+            println()
+        }
+    }
+
+    private fun printRoundWithName(roundResult: RacingRoundResult, carNames: List<String>) {
+        return roundResult.value.forEachIndexed { index, result ->
+            println("${carNames[index]} : ${"-".repeat(result)}")
+        }
+    }
+
+    private fun printWinnerNames(racingResult: RacingResult) {
+        val winnerText = racingResult.winnerNames.joinToString(separator = ", ")
+        println("$winnerText 이(가) 최종 우승했습니다.")
     }
 }
